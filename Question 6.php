985a990f030fa7c1799d67c9c655c83f0be6f494
sapnakumari-ops <sapnakumari748840@gmail.com>
2026-02-21 03:27:44 +0530

    Add files via upload

---
+++ Question 6.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+        $p = 1000;  
+         $r = 5;     
+         $t = 2;     
+        $in = $p * $r * $t;
+        $si= $in/100;
+        echo "Simple Interest = ".$si;
+       ?>
+
+</body>
+</html>
