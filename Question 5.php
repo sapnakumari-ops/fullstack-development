90a060e09df4425bdfcc9252ae3e8c512d5893ac
sapnakumari-ops <sapnakumari748840@gmail.com>
2026-02-21 03:27:23 +0530

    Add files via upload

---
+++ Question 5.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+       $length = 12;
+        $breadth = 6 ;
+        $area = $length * $breadth;
+        echo "Area of Rectangle = $area";
+      ?>
+
+</body>
+</html>
